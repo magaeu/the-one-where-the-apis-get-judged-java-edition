f73612f3fe0ce78ab1204e42c3ee032c4afb102d
Marcela Oliveira <magaeu@gmail.com>
2026-04-08 06:55:48 +0000

    remove lombok from credentials and create a simple credentials pojo

--- src/test/java/com/restfulbooker/api/dto/Credentials.java
+++ src/test/java/com/restfulbooker/api/dto/Credentials.java
@@ -1,17 +1,29 @@
 package com.restfulbooker.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
 public class Credentials {
 
-    @JsonProperty("username")
     private String username;
-    @JsonProperty("password")
     private String password;
+
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
--- src/test/java/com/restfulbooker/api/tests/AuthTest.java
+++ src/test/java/com/restfulbooker/api/tests/AuthTest.java
@@ -11,16 +11,18 @@ import com.restfulbooker.api.dto.Credentials;
 import com.restfulbooker.api.dto.Token;
 import com.restfulbooker.api.setup.BaseTest;
 
+import io.restassured.response.Response;
+
 public class AuthTest extends BaseTest {
 
     @Test
     @DisplayName("Generate auth token")
     public void generateAuthToken() {
 
-        Credentials credentials = Credentials.builder()
-            .username("admin")
-            .password("password123")
-            .build();
+        Credentials credentials = new Credentials(  
+                "admin",
+                "password123"
+        );
 
         Token responseToken = given()
                 .spec(getReq())
@@ -41,18 +43,24 @@ public class AuthTest extends BaseTest {
     @DisplayName("Generate auth token with invalid credentials")
     public void generateAuthTokenWithInvalidCredentials() {
 
-        Credentials invalidCredentials = Credentials.builder()
-                .username("invalidUser")
-                .password("invalidPass")
-                .build();
+        Credentials invalidCredentials = new Credentials(  
+                "invalidUser",
+                "invalidPass"
+        );
 
-        given()
+        Response response = given()
                 .spec(getReq())
                 .when()
                 .body(invalidCredentials)
                 .post("/auth")
                 .then()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+
+        String errorMessage = response.jsonPath().getString("reason");
+        assertThat(errorMessage).as("Error message is 'Bad credentials'").isEqualTo("Bad credentials");
+        
     }
 
 }
